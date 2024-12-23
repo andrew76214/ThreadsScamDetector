dcd9b7b743e8e0cc2436aa8172e091a5798c35a4
andrew76214 <liu76214@gmail.com>
2024-12-23 20:45:26 +0800

    [feat]: finish coordinator and judge

--- Agentic_AI/v1/agents.py
+++ Agentic_AI/v1/agents.py
@@ -10,7 +10,8 @@ class judge:
             template="""
             問題: {question} 
             貼文文字: {post_text} 
-            回答: 
+            請回答是否為詐騙？你的原因？
+            'button': '是/否', 'reason': '你的回答'
             """,
             input_variables=["question", "post_text"],
         )
@@ -19,6 +20,46 @@ class judge:
         self.post_text = post_text
         
     def response(self):
-        response = (self.pipeline.invoke({"question": self.question, "post_text": self.post_text}))['content']
+        response = self.pipeline.invoke({"question": self.question, "post_text": self.post_text})
+        print(response)
+        
+        if response == {}:
+            return {"error": str("LLM didn\'t response")}
+        
+        response = response["reason"]
+        response = filter_text(response)
+        return response
+    
+class coordinator:
+    def __init__(self, judge_response, question, post_text):
+        self.summary_prompt = PromptTemplate(
+            template="""
+            問題: {question} 
+            貼文文字: {post_text}
+            
+            以下是兩個代理的回應：
+            第一個代理的回應: {judge_1} 
+            第二個代理的回應: {judge_2}
+            根據以上內容，請統整兩個回應，請回答是否為詐騙？你的原因？
+            使用以下這個格式
+            'button': '是/否', 'reason': '你的回答'
+            """,
+            input_variables=["question", "post_text", "judge_1", "judge_2"],
+        )
+        self.pipeline = self.summary_prompt | LOCAL_LLM | JsonOutputParser()
+        self.judge_response = judge_response
+        self.question = question
+        self.post_text = post_text
+        
+    def response(self):
+        judge_1, judge_2 = self.judge_response
+        print(self.judge_response[0])
+        response = self.pipeline.invoke({"question": self.question, "post_text": self.post_text, "judge_1": judge_1, "judge_2": judge_2})
+        print(response)
+        
+        if response == {}:
+            return {"error": str("LLM didn\'t response")}
+
+        response = response["reason"]
         response = filter_text(response)
         return response
